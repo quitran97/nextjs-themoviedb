b468524ab1fa0a87ccefd1a35b3edab45095fc4a
hoangqui <hoangqui97ctm@gmail.com>
2022-04-02 16:25:34 +0700

    origin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
-import "normalize.css/normalize.css";
 import "../styles/globals.css";
+import "normalize.css/normalize.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
